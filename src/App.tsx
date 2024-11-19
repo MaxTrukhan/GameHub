64a120fd733d1529662ab51aa661e6e1694c4076
Maks Trukhan <maxcodejs@gmail.com>
2024-11-19 10:41:24 -0600

    baseAside

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react"
+import { Grid, GridItem } from "@chakra-ui/react"
+
 
 function App() {
   return <Grid templateAreas={
     {
       base: `"nav" "main"`,
-      md: `"nav nav" "aside main"`
+      lg: `"nav nav" "aside main"`
     }
   }>
-    <GridItem area='nav' bg='coral'>
+    <GridItem  area='nav' bg='coral'>
       Nav
     </GridItem>
-    <Show above='md'>
-      <GridItem area='asbide' bg='red'>
-        Aside
-      </GridItem>
-    </Show>
-    
+        <GridItem  
+          area="aside"
+          bg="red"
+          display={{base: 'none', lg: 'block'}}
+         >
+          Aside
+        </GridItem>
     <GridItem area='main' bg='pink'>
       Main
     </GridItem>
