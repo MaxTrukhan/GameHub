9e72436523e9794d2a8d0422b6f585246a3fa855
Maks Trukhan <maxcodejs@gmail.com>
2024-12-03 16:04:10 -0600

    no img

--- src/service/optimizedImgs.ts
+++ src/service/optimizedImgs.ts
@@ -1,5 +1,7 @@
+import placeholderImg from '../assets/logo.webp'
+
 const GetCroppedImgUrl = (url: string) => {
-    if(!url) return ''
+    if(!url) return placeholderImg
     const target = 'media/'
     const index = url.indexOf(target) + target.length
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index)
